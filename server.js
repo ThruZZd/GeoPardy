082a1c5dd8b52f0022aab217c4ad5f514d332e92
Tobias <thruzzd@gmail.com>
2024-05-18 22:41:03 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const port = 8080
+var uArray = []
+
+app.use(express.static('public'))
+app.use(express.json())
+
+app.get('/fetch', (req, res)=>{
+
+res.status(200).send(JSON.stringify(uArray))
+
+
+})
+
+app.get('/reset', (req, res)=>{
+
+    res.status(200).send('')
+    uArray=[]
+    console.log('reset')
+    
+    
+    })
+
+app.post('/uinput',(req,res)=>{
+
+const parcel = req.body
+uArray.push(parcel['parcel'])
+
+
+if(!parcel){
+    res.status(400).send('<h1>Error!</h1>')
+}
+res.status(200).send('<h1>Danke</h1>')
+
+})
+
+
+app.listen(port, ()=>{
+    console.log('Server listening to '+port)
+})
+
+
